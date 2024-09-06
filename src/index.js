cd903a615f1c138db92fed5a38ad80c5274005e0
Jeditale <69350223+Jeditale@users.noreply.github.com>
2024-09-07 00:09:27 +0700

    ADD

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import './features/navbar.js';
+import Navbar from './features/navbar.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
